31d3875fdd0bd36e6b7e34bd648578ac2fac39a0
Diego Vasquez <122320757+davm07@users.noreply.github.com>
2026-03-27 23:43:35 +0100

    add framer motion

--- apps/web/app/login/page.tsx
+++ apps/web/app/login/page.tsx
@@ -8,69 +8,93 @@ import {
 import { User, Eye } from "lucide-react";
 import { Input } from "../../components/Input";
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 export default function Login() {
   const [mode, setMode] = useState<"login" | "signup">("login");
 
   return (
-    <main className="flex bg-athens-gray-50 h-screen p-4 gap-24">
+    <main
+      className={`flex h-screen p-4 gap-24
+      ${mode === "signup" ? "flex-row-reverse" : ""}
+      `}
+    >
       <div className="flex justify-center flex-col flex-1 gap-12 items-center">
         <h1 className="font-heading text-4xl text-athens-gray-950 text-left w-96">
           {mode === "login" ? "Log In" : "Create an account"}
         </h1>
 
-        <form className="flex flex-col gap-8 w-96">
-          <label
-            htmlFor="username"
-            className="text-athens-gray-950 font-semibold"
-          >
-            Username
-            <Input
-              icon={User}
-              type="text"
-              placeholder="Username"
-              id="username"
-            />
-          </label>
-          <label
-            htmlFor="password"
-            className="text-athens-gray-950 font-semibold"
-          >
-            Password
-            <Input
-              icon={Eye}
-              type="password"
-              placeholder="Password"
-              id="password"
-            />
-          </label>
-          {mode === "signup" && (
+        <motion.div
+          layout
+          transition={{ duration: 0.5 }}
+          className="flex justify-center flex-col flex-1 gap-12 items-center"
+        >
+          <form className="flex flex-col gap-8 w-96">
             <label
-              htmlFor="confirm-password"
+              htmlFor="username"
               className="text-athens-gray-950 font-semibold"
             >
-              Confirm Password
+              Username
+              <Input
+                icon={User}
+                type="text"
+                placeholder="Username"
+                id="username"
+              />
+            </label>
+            <label
+              htmlFor="password"
+              className="text-athens-gray-950 font-semibold"
+            >
+              Password
               <Input
                 icon={Eye}
                 type="password"
-                placeholder="Confirm Password"
-                id="confirm-password"
+                placeholder="Password"
+                id="password"
               />
             </label>
-          )}
+            {mode === "signup" && (
+              <label
+                htmlFor="confirm-password"
+                className="text-athens-gray-950 font-semibold"
+              >
+                Confirm Password
+                <Input
+                  icon={Eye}
+                  type="password"
+                  placeholder="Confirm Password"
+                  id="confirm-password"
+                />
+              </label>
+            )}
+            <button
+              type="submit"
+              className="bg-roman-500 px-2 py-3 rounded-lg font-bold font-heading text-lg tracking-wider hover:bg-roman-400 transition-all"
+            >
+              {mode === "login" ? "Log In" : "Sign Up"}
+            </button>
+          </form>
+        </motion.div>
+
+        <p className="text-sm text-roman-800 text-center w-96">
+          {mode === "login"
+            ? "Don't have an account? "
+            : "Already have an account? "}
           <button
-            type="submit"
-            className="bg-roman-500 px-2 py-3 rounded-lg font-bold font-heading text-lg tracking-wider hover:bg-roman-400 transition-all"
+            type="button"
+            className="text-roman-900 hover:underline transition-all font-semibold"
+            onClick={() => setMode(mode === "login" ? "signup" : "login")}
           >
-            {mode === "login" ? "Log In" : "Sign Up"}
+            {mode === "login" ? "Sign up" : "Log in"}
           </button>
-        </form>
+        </p>
 
         <div className="flex flex-col gap-4 w-96">
           <div className="flex flex-row items-center gap-2">
             <div className="flex-1 h-px bg-athens-gray-950"></div>
             <p className="text-athens-gray-950 font-heading text-lg">
-              or continue with
+              {mode === "login" ? "or continue with" : "or sign up with"}
             </p>
             <div className="flex-1 h-px bg-athens-gray-950"></div>
           </div>
@@ -92,18 +116,24 @@ export default function Login() {
         </div>
       </div>
 
-      <div className="w-full h-full bg-roman-400 flex flex-col items-center justify-center p-10 flex-1 rounded-2xl gap-8">
-        <LoginSvg className="h-5/6 w-full" />
-        <div>
-          <p className="text-2xl font-bold font-heading tracking-wider">
-            Upload your study materials.
-          </p>
-          <p>
-            Get AI-powered summaries and a personalized study plan based on your
-            exam dates.
-          </p>
+      <motion.div
+        layout
+        transition={{ duration: 0.5 }}
+        className="w-full h-full bg-roman-400 flex flex-col items-center justify-center p-10 flex-1 rounded-2xl gap-8"
+      >
+        <div className="w-full h-full bg-roman-400 flex flex-col items-center justify-center p-10 flex-1 rounded-2xl gap-8">
+          <LoginSvg className="h-5/6 w-full" />
+          <div>
+            <p className="text-2xl font-bold font-heading tracking-wider">
+              Upload your study materials.
+            </p>
+            <p>
+              Get AI-powered summaries and a personalized study plan based on
+              your exam dates.
+            </p>
+          </div>
         </div>
-      </div>
+      </motion.div>
     </main>
   );
 }
